a85449decc304f23755ab7bcf7f3cf4e6e0e6983
Volodymyr Tieliezhkin <57314700+Volodymyr-T@users.noreply.github.com>
2022-01-06 00:38:05 +0100

    feat: write a toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    const Ones = ['', 'one', 'two', 'three', 'four', 'five',
+                  'six', 'seven', 'eight', 'nine', 'ten',
+                  'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 
+                  'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    const Tens = ['','','twenty', 'thirty', 'forty', 'fifty', 
+                  'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    const numArray = number.toString().split('');   // Create an Array of char
+    const one = +numArray.pop() || 0;               // A number of ones
+    const ten = +numArray.pop() || 0;               // A number of tens
+    const hundred = +numArray.pop() || 0;           // A number of hundreds
+    
+    if (number === 0) return 'zero';                // A special case
+    return `${
+                hundred ? Ones[hundred] + ' hundred' : ''
+            } ${
+                (ten < 2) ? Ones[Number(''+ten+one)] : Tens[ten] + ' ' + Ones[one]
+            }`.trim();
 }
+
